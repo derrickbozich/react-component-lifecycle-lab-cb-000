c95563046a905fac8312cec98cbac8077b1cb151
derrick b <dbozich1@gmail.com>
2018-10-26 01:13:30 +0000

    automatically backed up by learn

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -10,9 +10,18 @@ class TweetWall extends React.Component {
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  // todo: componentWillMount()
+  componentWillMount(){
+    this.setState({tweets: this.props.newTweets})
+  }
+  // todo: shouldComponentUpdate()
+  shouldComponentUpdate(nextProps){
+    return nextProps.newTweets.length > 0
+  }
+  // todo: componentWillReceiveProps()
+  componentWillReceiveProps(){
+    this.setState({tweets: this.props.newTweets})
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
